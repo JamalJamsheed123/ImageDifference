68ed58831890735c452450c40cfb26ea1d3835e8
JamalJamsheed123 <jamal.jamsheed@gmail.com>
2023-04-07 21:07:16 +0500

    Update StartActivity
    ----> On backPress Exit the App

--- app/src/main/java/com/yesnet/imagediff/StartActivity.kt
+++ app/src/main/java/com/yesnet/imagediff/StartActivity.kt
@@ -1,12 +1,16 @@
 package com.yesnet.imagediff
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity()
 {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +20,15 @@ class StartActivity : AppCompatActivity()
             val  intent = Intent(this, LevelScreen::class.java)
             startActivity(intent)
         }
-
-        removeActionBar()
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        // Finish all activities in stack and app closes
+        Toast.makeText(this@StartActivity, "exit the app", Toast.LENGTH_SHORT).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            kotlin.run {
+                finishAffinity()}
+        }, 2000)
     }
 
-    private fun removeActionBar(){
-
-        supportActionBar?.hide()
-    }
 }
